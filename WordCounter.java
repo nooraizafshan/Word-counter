feff1ec4a8063af69f785a3c7350c30faec04f9d
Afshan_Noreen <126900173+nooraizafshan@users.noreply.github.com>
2023-12-30 23:58:57 +0500

    Add files via upload

---
+++ WordCounter.java
@@ -0,0 +1,14 @@
+
+package wordcounter;
+
+
+public class WordCounter {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        counter counter=new counter();
+        counter.setVisible(true);
+    }
+    
+}
